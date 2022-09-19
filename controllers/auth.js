80af27e862271ab9a53e598b6a4e6160f3c9a626
OrlandoMts <dani_monts19@hotmail.com>
2022-09-18 21:32:27 -0500

    Hice que el controlador registre un nuevo usuario con un hash de la contrasenna

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,47 @@
 const { response, json } = require('express');
-const createUser = (req, res = response) => {
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario'
-    });
+const createUser = async (req, res = response) => {
+
+    const {name, email, password} = req.body
+
+    try {
+        // Verificar email
+        let usuario = await Usuario.findOne({email: email});
+        if(usuario){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        //Crear usuario con el modelo
+        usuario = new Usuario( req.body );
+
+        // Hash de la contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        // Guardar usuario en la bd
+        await usuario.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Contactar al administrador'
+        });
+    }
+
+    
 };
 
 const login = (req, res = response) => {
